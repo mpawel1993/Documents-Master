4ab166c8131e3f65de252f3b97717b2deef7b26b
Pawel Mazur <mpawel1993@gmail.com>
2020-12-19 23:55:58 +0100

    SpringBoot application prototype added #1

---
+++ src/test/java/pl/mazur/pawel/DocumentsMaster/DocumentsMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mazur.pawel.DocumentsMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
