7abb9e97330297c59fe34f45abf8aa9d2ab303a7
Pawel Mazur <mpawel1993@gmail.com>
2020-12-20 14:48:02 +0100

    PrimeNg angular dependency added #5

--- src/main/angular/documents-master-ui/src/app/app.component.ts
+++ src/main/angular/documents-master-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'documents-master-ui';
+
+  constructor(private messageService: MessageService) {
+  }
+
+  showNegativeMessage() {
+    this.messageService.add({severity:'error', summary:'PrimeNg', detail:'Hello your PrimeNg'});
+  }
+
+  showPositiveMessage() {
+    this.messageService.add({severity:'success', summary:'PrimeNg', detail:'Hello your PrimeNg'});
+  }
 }
