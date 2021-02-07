9854cb3b18362b5f0b1a4c5ef6fef929b8f3f0ce
Pawel Mazur <mpawel1993@gmail.com>
2021-02-07 10:32:18 +0100

    Refactored wrong code #16

--- src/main/angular/documents-master-ui/src/app/add-document/add-document.component.ts
+++ src/main/angular/documents-master-ui/src/app/add-document/add-document.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {MessageService} from "primeng/api";
-import {Document} from "../domain/document";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { randomInt } from 'crypto';
+import { MessageService } from "primeng/api";
+import { Document } from "../domain/document";
 
 @Component({
   selector: 'app-add-document',
@@ -10,45 +11,22 @@ import {Document} from "../domain/document";
 })
 export class AddDocumentComponent implements OnInit {
 
-  inputForm = new FormGroup({});
-
-  document: Document | undefined
-
-  //todo add flags
-  cities = ['', 'Poland', 'Germany', 'Netherlans'];
-  values: string[] = ['Java', 'C#'];
-  results: string[] = [];
-  tagValue: string = '';
+  addDocumentForm = new FormGroup({});
 
   constructor(private fb: FormBuilder, private messageService: MessageService) {
   }
 
-  search(event: any) {
-    this.results = ['a', 'b', 'c']
-  }
-
-  ngOnInit(): void {
-    this.inputForm = this.fb.group({
-      name: '',
-      issuer: '',
-      creationDate: '',
-      uploadDate: '',
-      country: '',
-      folder: '',
-      tags: ''
+  ngOnInit() {
+    this.addDocumentForm = this.fb.group({
+      documentName: '',
+      createDate: new Date(),
+      tags: []
     });
-
-    this.inputForm.valueChanges.subscribe(console.log)
-  }
-
-  displaySubmit() {
-    this.document = this.inputForm.value;
-    console.log('Provided document ', this.document);
-    this.messageService.add({severity: 'success', summary: 'Submitted', detail: 'Submitted'});
   }
 
   addInput() {
-    this.values.push(this.tagValue);
-
+    this.addDocumentForm.patchValue({
+      tags: ['Ala', 'Ma', 'Kota']
+    });
   }
 }
