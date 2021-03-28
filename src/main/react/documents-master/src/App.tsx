4d2508fd6b13cb897837212f78316c03323918dc
Pawel Mazur <mpawel1993@gmail.com>
2021-03-28 13:40:18 +0200

    #21 Light theme provided and background changed

--- src/main/react/documents-master/src/App.tsx
+++ src/main/react/documents-master/src/App.tsx
@@ -1,6 +1,5 @@
-import React, {useState} from 'react';
 import './App.css';
-import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import {Footer} from "./footer/Footer";
